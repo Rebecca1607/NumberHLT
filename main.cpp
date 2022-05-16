0041b6ef91312535ff666b432d83bf4d7c07b512
rebecca1990 <93472100+Rebecca1607@users.noreply.github.com>
2022-05-16 12:30:00 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout << "Hello, Welcome to double the number!\n";
+ 
+cout << "Enter a number  :  ";
+
+int anumber;
+cin >> anumber;
+
+cout << "Double that number is :\n"<<anumber*2;
+
+
+
+
+cout << "\nAdd them up!\n";
+
+cout <<"Enter three numbers:   ";
+int num1, num2, num3, sum;
+
+cin >> num1 >> num2 >> num3;
+
+sum = num1 + num2 + num3;
+
+cout << "Sum of "<< num1 << " and " << num2 << " and " << num3 << " is " << sum;
+
+
+
+}
